e1566adc95b3e9672f379a8761839f8ed4b51ca4
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-08-22 10:52:28 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,10 +3,24 @@ const TodoNew = (props) => {
     // console.log(">>> check point:", props)
     const { addNewTodo } = props;
     // addNewTodo("Duy");
+
+    // fire: khai hỏa / chạy 
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleInChange", name)
+    }
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
