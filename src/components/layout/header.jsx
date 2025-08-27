5bbee681ce762fa4863f4f0dc0182ee538ebe972
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-08-27 16:07:39 +0700

    video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import './header.css'
 
+// thẻ a(anchor) - href sẽ load khi chuyển trang
+// thẻ link - to không cần load
 const Header = () => {
     return (
         <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
+            <li><Link class="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/products">Products</Link></li>
         </ul>
     )
 }
