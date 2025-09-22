b23684b7680d59b71b5259296f46690346f24d9d
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-09-22 16:16:23 +0700

    video 76 - 77 - 78

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,17 +1,42 @@
 import { Link, NavLink } from 'react-router-dom';
-import './header.css'
+import { UserOutlined, HomeOutlined, ProductOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { useState } from 'react';
 
 // thẻ a(anchor) - href sẽ load khi chuyển trang
 // thẻ link - to không cần load
 // =>> NavLink sẽ tự động css class="active" nào từng trang điều hướng tương ứng
 const Header = () => {
+    const [current, setCurrent] = useState('');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: 'users',
+            icon: <UserOutlined />,
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: 'books',
+            icon: <ProductOutlined />,
+        },
+    ];
     return (
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/users">Users</NavLink></li>
-            <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items} />
     )
 }
 
-export default Header;
+export default Header; 
