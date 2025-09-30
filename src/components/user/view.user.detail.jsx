178f5f3481a6eaba64aa3ca6c4bb6004cb435230
NguyenTranKhaiDuy <nguyentrankhaiduy@gmail.com>
2025-09-30 16:51:01 +0700

    video 84

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,12 +1,14 @@
-import { Button, Drawer } from "antd";
+import { Button, Drawer, notification } from "antd";
 import { useState } from "react";
+import { handleUploadFile, updateUserAPI, updateUserAvatarAPI } from "../../services/api.service";
 
 const ViewUserDetail = (props) => {
     const {
         dataDetail,
         setDataDetail,
         isDetailOpen,
-        setIsDetailOpen
+        setIsDetailOpen,
+        loadUser
     } = props;
 
     const [selectedFile, setSelectedFile] = useState(null)
@@ -26,7 +28,43 @@ const ViewUserDetail = (props) => {
             setPreview(URL.createObjectURL(file))
         }
     }
-    console.log("check:", preview)
+
+    const handleUpdateUserAvatar = async () => {
+        // step 1: upload file
+        const resUpload = await handleUploadFile(selectedFile, "avatar");
+        if (resUpload.data) {
+            // success
+            const newAvatar = resUpload.data.fileUploaded;
+            // step 2: update user
+            const resUpdateAvatar = await updateUserAvatarAPI(
+                newAvatar, dataDetail._id, dataDetail.fullName, dataDetail.phone
+            );
+            if (resUpdateAvatar.data) {
+                setIsDetailOpen(false);
+                setSelectedFile(null);
+                setPreview(null);
+                await loadUser();
+
+                notification.success({
+                    message: "Update user avatar",
+                    description: "Cập nhật ảnh thành công"
+                })
+
+            } else {
+                notification.error({
+                    message: "Error update avatar",
+                    description: JSON.stringify(resUpdateAvatar.message)
+                })
+            }
+        } else {
+            // failed
+            notification.error({
+                message: "Error upload file",
+                description: JSON.stringify(resUpload.message)
+            })
+        }
+
+    }
 
     return (
         <Drawer
@@ -76,14 +114,19 @@ const ViewUserDetail = (props) => {
                 </div>
 
                 {preview &&
-                    <div style={{
-                        marginTop: "10px",
-                        height: "150px", width: "150px",
-                        border: "1px solid #ccc"
-                    }}>
-                        <img style={{ height: "100%", width: "100%", objectFit: "contain" }}
-                            src={preview} />
-                    </div>
+                    <>
+                        <div style={{
+                            marginTop: "10px",
+                            marginBottom: "15px",
+                            height: "150px", width: "150px",
+                        }}>
+                            <img style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                                src={preview} />
+                        </div>
+                        <Button type='primary'
+                            onClick={() => handleUpdateUserAvatar()}
+                        >Save</Button>
+                    </>
                 }
             </>
                 :
